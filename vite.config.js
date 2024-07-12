78a08fed789ca1e0add1af362168418456d87baf
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-07-12 09:55:47 -0400

    changed resolve

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 // import envPlugin from "vite-plugin-env";
+import path from "path";
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
+  },
 });
