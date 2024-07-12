7e2c8fd7776f582e97e7fda78b75892ad26e2b34
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-07-12 10:08:58 -0400

    external edit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
-// import envPlugin from "vite-plugin-env";
-import path from "path";
 
 dotenv.config();
 
@@ -11,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [/^@:.*/],
+      external: [/^node:.*/],
     },
   },
 });
